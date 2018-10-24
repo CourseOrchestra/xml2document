9a30c77edd049f622472d215ee5be42179fb549b
s.borodanev <boss@curs.ru>
2018-10-24 11:08:19 +0300

    Removed unnecessary imports and exceptions that could be thrown

--- src/main/java/ru/curs/xml2document/test/XML2DocTests.java
+++ src/main/java/ru/curs/xml2document/test/XML2DocTests.java
@@ -3,23 +3,16 @@ package ru.curs.xml2document.test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.*;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
-import org.xml.sax.SAXException;
-import ru.curs.xml2document.DOCXReportWriter;
-import ru.curs.xml2document.OutputType;
 import ru.curs.xml2document.XML2Document;
 import ru.curs.xml2document.XML2WordError;
-import java.io.File;
 
 
 public class XML2DocTests {
 
     @Test
-    void saxDataReaderTest() throws FileNotFoundException, IOException, XML2WordError {
+    void saxDataReaderTest() {
         assertThrows(XML2WordError.class, () ->
         XML2Document.processUsingSax("filesForTesting/dataXML.xml", "filesForTesting/descriptorXML.xml",
                 "filesForTesting/template.docx", "filesForTesting/resultForSAX.docx")
@@ -27,7 +20,7 @@ public class XML2DocTests {
     }
 
     @Test
-    void resultFileExistenceCheck() throws FileNotFoundException, IOException, XML2WordError {
+    void resultFileExistenceCheck() throws IOException, XML2WordError {
         XML2Document.process("filesForTesting/dataXML.xml", "filesForTesting/descriptorXML.xml",
                 "filesForTesting/template.docx", "filesForTesting/resultTestFile.docx");
         File resultTestFile = new File("filesForTesting/resultTestFile.docx");
@@ -36,7 +29,7 @@ public class XML2DocTests {
     }
 
     @Test
-    void resultFileExistenceCheck2() throws FileNotFoundException, IOException, XML2WordError {
+    void resultFileExistenceCheck2() throws IOException, XML2WordError {
         XML2Document.process(new FileInputStream("filesForTesting/dataXML.xml"), new FileInputStream("filesForTesting/descriptorXML.xml"),
                 new FileInputStream("filesForTesting/template.docx"), new FileOutputStream("filesForTesting/resultTestFile2.docx"));
         File resultTestFile = new File("filesForTesting/resultTestFile2.docx");
@@ -45,7 +38,7 @@ public class XML2DocTests {
     }
 
     @Test
-    void docResultFileImplementationError() throws FileNotFoundException, IOException, XML2WordError {
+    void docResultFileImplementationError() throws IOException, XML2WordError {
                 XML2Document.process("filesForTesting/dataXML.xml", "filesForTesting/descriptorXML.xml",
                         "filesForTesting/template.docx", "filesForTesting/resultTestFile3.doc");
         File resultTestFile = new File("filesForTesting/resultTestFile3.docx");
